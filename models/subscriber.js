55a5b9e0191f2f3ef84a8b18957bc07555041fb1
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-30 14:15:46 -0600

    expanded subscriber model

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose"),
     subscriberSchema = mongoose.Schema({
-        name: String,
-        email: String,
-        zipCode: Number
-    });
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowecase: true,
+            unique: true
+        },
+        zipCode: {
+            type: Number,
+            min: [10000, "Zip code must be 5 digits"],
+            max: 9999
+        },
+        courses: [{type: mongoose.Schema.Types.ObjectId, ref: Course}]
+    },
+    {
+        timestamps: true
+    }
+    );
+
+subscriberSchema.methods.getInfo = function () {
+    return `Name: ${this.name} Email: ${this.email} Zipcode: ${this.zipCode}`;
+}
 
 module.exports = mongoose.model("Subscriber", subscriberSchema);
