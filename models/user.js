e9eee086e6f2d60ffa259c324ed2b08219d66611
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-30 14:56:06 -0600

    finished models

--- models/user.js
+++ models/user.js
@@ -35,4 +35,28 @@ userSchema = new Schema(
     }
 )
 
+userSchema.virtual("fullName").get(function {
+    return `${this.name.first} ${this.name.last}`
+});
+
+userSchema.pre("save", function {
+    let user = this;
+    if(user.subscribedAccount == undefined) {
+        Subscriber.findOne({
+            email: user.email
+        })
+        .then(subscriber => {
+            user.subscribedAccount = subscriber;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error in associating subscriber: ${error.message}`);
+            next(error);
+        })
+    }
+    else {
+        next();
+    }
+})
+
 module.exports = mongoose.model("User", userSchema);
